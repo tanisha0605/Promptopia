60fda0fc5b3f598152a7afd18d0c99cb87710437
tanisha0605 <tanishakanal@gmail.com>
2024-02-14 20:21:47 +0530

    Bug fix  commit

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -45,10 +45,11 @@ export const DELETE = async (request, { params }) => {
         await connectToDB();
 
         // Find the prompt by ID and remove it
-        await Prompt.findByIdAndRemove(params.id);
+        await Prompt.findByIdAndDelete(params.id);
 
         return new Response("Prompt deleted successfully", { status: 200 });
     } catch (error) {
+        console.log(error);
         return new Response("Error deleting prompt", { status: 500 });
     }
 };
